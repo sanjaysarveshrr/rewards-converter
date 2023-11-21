d9bae4817427bcae09902cb68f2a5fd906e1e4cf
Sanjaysarvesh <sanj.sarvesh6703@gmail.com>
2023-11-21 19:06:48 +0530

    done

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,23 +2,19 @@ public class RewardValue {
     private double cashValue;
     private int milesValue;
 
-    // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
         this.milesValue = (int) (cashValue / 0.0035);
     }
 
-    
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
         this.cashValue = milesValue * 0.0035;
     }
-
     
     public double getCashValue() {
         return cashValue;
     }
-
     
     public int getMilesValue() {
         return milesValue;
--- src/test/java/RewardValueTest.java
+++ src/test/java/RewardValueTest.java
@@ -2,9 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RewardValueTests {
-
-    @Test
+public class RewardValueTest {
+@Test
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
@@ -20,11 +19,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 200;
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMilesValue = (int) (cashValue / 0.0035);      
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 5000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue * 0.0035;
+        assertEquals(expectedCashValue, rewardValue.getCashValue(), 0.0001);
     }
 }
